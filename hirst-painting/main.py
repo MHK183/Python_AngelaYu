73b8abf1ebbec6fa72d260c9fd28c95e4b9839a8
Kim Min Hyeok <4438mh@naver.com>
2023-08-30 18:21:22 +0900

    Day 18 Project

---
+++ hirst-painting/main.py
@@ -0,0 +1,28 @@
+import random
+import turtle as t
+
+tim = t.Turtle()
+screen = t.Screen()
+t.colormode(255)
+color_list = [(35, 19, 15), (197, 144, 123), (30, 106, 155), (142, 85, 55), (9, 21, 45), (236, 213, 85), (196, 135, 155), (156, 62, 90), (221, 85, 66), (153, 17, 37), (202, 79, 104), (14, 54, 30), (162, 163, 35), (118, 172, 196), (41, 125, 79), (77, 12, 22), (120, 188, 160), (18, 92, 53), (11, 58, 137), (23, 201, 179), (23, 174, 206), (139, 222, 208), (149, 23, 16), (223, 172, 191), (233, 172, 162), (238, 206, 8)]
+tim.speed("fastest")
+tim.hideturtle()
+tim.penup()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+number_of_dots = 100
+for i in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if i % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen.exitonclick()
