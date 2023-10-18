b55eb32288499766469ba94c3bf120edec9121e7
Kim Min Hyeok <4438mh@naver.com>
2023-10-18 15:51:14 +0900

    Day-30

---
+++ day-30/main.py
@@ -0,0 +1,26 @@
+# # FileNotFound
+#
+# try:
+#     file = open("a_file.txt")
+#     a = {"key": "value"}
+#     print(a["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("this is an error that I made up.")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+
+bmi = weight / height ** 2
+print(bmi)
