70f634398b7b45a3edb8de09b8d7d207fbef8313
Kim Min Hyeok <4438mh@naver.com>
2023-10-19 16:02:10 +0900

    passwordmanager

---
+++ flash-card-project-start/main.py
@@ -0,0 +1,15 @@
+from tkinter import Tk, PhotoImage, Button
+
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# UI
+window = Tk()
+
+# image load
+card_back_image = PhotoImage(file="images/card_back.png")
+card_front_image = PhotoImage(file="images/card_front.png")
+right_image = PhotoImage(file="images/right.png")
+wrong_image = PhotoImage(file="images/wrong.png")
+
